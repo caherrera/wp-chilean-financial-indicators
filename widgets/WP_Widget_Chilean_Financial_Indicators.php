f6d7c69b46afd8957500e3a12dfe0d5c32698674
Carlos Herrera <carlos.herrera@itlinesoftware.com>
2017-11-20 14:34:05 -0300

    finish widget

--- widgets/WP_Widget_Chilean_Financial_Indicators.php
+++ widgets/WP_Widget_Chilean_Financial_Indicators.php
@@ -8,20 +8,53 @@
 
 class WP_Widget_Chilean_Financial_Indicators extends WP_Widget_Chilean_Indicators
 {
-    public function __construct() {
+    public $apiUrl = 'http://www.mindicador.cl/api';
+
+    public function __construct()
+    {
+
 
         parent::__construct(
             'chilean-financial-indicators', //ID
             'Chilean Financial Indicators', //Nombre
             array(
-                'classname' => 'widget_chilean_financial_indicators',
+                'classname'   => 'widget_chilean_financial_indicators',
                 'description' => ''
             )
         );
     }
 
+    public function widget($args, $instance)
+    {
+        echo '<div class="WP_Widget_Chilean_Indicators WP_Widget_Chilean_Financial_Indicators">';
+        echo '<ul>';
+        echo $this->printUF();
+        echo $this->printDollar();
+        echo '</ul>';
+        echo '</div>';
+
+    }
+
+    public function printUF()
+    {
+        $key   = 'uf';
+        $label = 'uf';
+        $value = money_format("%n", $this->data()->uf->valor);
+
+        return $this->printValue($key, $value, $label);
+    }
 
 
+    public function printDollar()
+    {
+        $key   = 'dolar';
+        $label = 'Dolar OBS.';
+
+        $value = money_format("%n", $this->data()->dolar->valor);
+
+        return $this->printValue($key, $value, $label);
+
+    }
 
 
 }
--- widgets/WP_Widget_Chilean_Indicators.php
+++ widgets/WP_Widget_Chilean_Indicators.php
@@ -9,6 +9,13 @@
 class WP_Widget_Chilean_Indicators extends WP_Widget
 {
     public $apiUrl = 'http://www.mindicador.cl/api';
+    public $data = null;
+
+    public function __construct($id_base, $name, array $widget_options = array(), array $control_options = array())
+    {
+        setlocale(LC_MONETARY, 'es_CL.utf8');
+        parent::__construct($id_base, $name, $widget_options, $control_options);
+    }
 
     public function update($new_instance, $old_instance)
     {
@@ -17,8 +24,73 @@ class WP_Widget_Chilean_Indicators extends WP_Widget
 
     public function widget($args, $instance)
     {
-        $data=$this->fetch();
+        $data = $this->fetch();
         echo json_encode($data);
+    }
+
+    public function fetch()
+    {
+
+        $cacheFile = $this->getCacheFile();
+        if ( ! is_dir(dirname($cacheFile))) {
+            mkdir(dirname($cacheFile));
+        }
+        if (file_exists($cacheFile)) {
+            $cache = file_get_contents($cacheFile);
+            if ($cache) {
+                return json_decode($cache);
+            }
+        }
+        $response = $this->sync($this->apiUrl);
+        file_put_contents($cacheFile, $response);
+
+        return json_decode($response);
+
+
+    }
+
+    public function getCacheFile($sufix = '')
+    {
+        $today    = date('Y-m-d');
+        $filename = [__CLASS__, $today];
+        if ($sufix) {
+            $filename[] = $sufix;
+        }
+        $filename[] = 'cache';
+        $cacheFile  = dirname(dirname(__FILE__)) . '/cache/' . implode('.', $filename);
+
+        return $cacheFile;
+    }
+
+    /**
+     * @return string
+     */
+    public function sync()
+    {
+
+        if ( ! $this->apiUrl) {
+            return '';
+        }
+        if (ini_get('allow_url_fopen')) {
+            $json = file_get_contents($this->apiUrl);
+        } else {
+            // De otra forma utilizamos cURL
+            $curl = curl_init($this->apiUrl);
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+            $json = curl_exec($curl);
+            curl_close($curl);
+        }
+
+        return $json;
+    }
+
+    public function data()
+    {
+        if ( ! $this->data) {
+            $this->data = $this->fetch();
+        }
+
+        return $this->data;
     }
 
     public function form($instance)
@@ -42,37 +114,17 @@ class WP_Widget_Chilean_Indicators extends WP_Widget
 
     }
 
-    public function fetch()
+    /**
+     * @param string $key
+     * @param int $value
+     * @param string $label
+     *
+     * @return string
+     */
+    protected function printValue($key, $value, $label = null)
     {
 
-        $today     = date('Y-m-d');
-        $cacheFile = dirname(dirname(__FILE__)) . '/cache/' . __CLASS__ . '.'.$today.'.cache';
-        if (file_exists($cacheFile)) {
-            return json_decode(file_get_contents($cacheFile));
-        }
-        $response = $this->sync($this->apiUrl);
-        file_put_contents($cacheFile, $response);
-
-        return json_decode($response);
-
-
-    }
-
-    public function sync($apiUrl)
-    {
-
-
-        if (ini_get('allow_url_fopen')) {
-            $json = file_get_contents($apiUrl);
-        } else {
-            // De otra forma utilizamos cURL
-            $curl = curl_init($apiUrl);
-            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-            $json = curl_exec($curl);
-            curl_close($curl);
-        }
-
-        return $json;
+        return sprintf("<li class=\"%s\"><span class=\"unit\">%s</span> %s</li>", $key, $label, $value);
     }
 
 
